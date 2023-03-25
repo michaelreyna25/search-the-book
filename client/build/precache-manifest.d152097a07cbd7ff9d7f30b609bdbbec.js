0c279c9957bbdf41d9e1d2f2143f686f340fb786
Michael Reyna <25.27.reyna@gmail.com>
2023-03-24 22:53:40 -0600

    changed packagejson

---
+++ client/build/precache-manifest.d152097a07cbd7ff9d7f30b609bdbbec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6bd9d335115661cdac6df0542d98c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e2d166eb6b68379c3b2",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "73e2b39496b3e7d663a1",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "8e2d166eb6b68379c3b2",
+    "url": "/static/js/2.72753747.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.72753747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73e2b39496b3e7d663a1",
+    "url": "/static/js/main.ffd9b9b7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.ffd9b9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
+import Auth from "./utils/auth";
+import {
+	ApolloClient,
+	createHttpLink,
+	InMemoryCache,
+	ApolloProvider,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const http = createHttpLink({
+	uri: "/graphql",
+});
+
+const auth = setContext((_, { headers }) => {
+	const token = Auth.getToken();
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
+});
+
+const client = new ApolloClient({
+	link: auth.concat(http),
+	cache: new InMemoryCache(),
+});
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={SearchBooks} />
+						<Route exact path="/saved" component={SavedBooks} />
+						<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
